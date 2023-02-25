dbea018572ee6744c741f294d6a831cb57fe9233
L-glance <dark0pesto7@mail.ru>
2023-02-25 12:27:41 +0300

    Add Divide and Multi Poly

--- FF/FiniteFieldElement.cs
+++ FF/FiniteFieldElement.cs
@@ -43,7 +43,6 @@ namespace FF
         {
             if (!el1.field.Equals(el2.field))
                 throw new InvalidOperationException();
-            var field = el1.field;
             return null;
         }
         public static FiniteFieldElement operator *(FiniteFieldElement el1, FiniteFieldElement el2)
@@ -51,7 +50,49 @@ namespace FF
             if (!el1.field.Equals(el2.field))
                 throw new InvalidOperationException();
             var field = el1.field;
-            return null;
+            var multPoly = new int[el1.Poly.Length + el2.Poly.Length - 1];
+            for(var i = el1.Poly.Length - 1; i >=0 ;i--)
+            {
+                for(var j = el2.Poly.Length -1;j >= 0;j--)
+                {
+                    multPoly[i + j] += el1.Poly[i] * el2.Poly[j];
+                }
+            }
+            return new FiniteFieldElement(multPoly, field);
+        }
+        public static FiniteFieldElement operator %(FiniteFieldElement el1, FiniteFieldElement el2)
+        {
+            if (!el1.field.Equals(el2.field))
+                throw new InvalidOperationException();
+
+            var field = el1.field;
+            var dividend = el1.Poly;
+            var divisor = el2.Poly;
+
+            int dividendDegree = dividend.Length - 1;
+            int divisorDegree = divisor.Length - 1;
+
+            if (dividendDegree < divisorDegree)
+            {
+                return el1;
+            }
+
+            int[] quotient = new int[dividendDegree - divisorDegree + 1];
+            int[] remainder = new int[dividendDegree + 1];
+            Array.Copy(dividend, remainder, dividendDegree + 1);
+
+            for (int i = dividendDegree - divisorDegree; i >= 0; i--)
+            {
+                quotient[i] = remainder[i + divisorDegree] / divisor[divisorDegree];
+
+                for (int j = i + divisorDegree; j >= i; j--)
+                {
+                    remainder[j] -= quotient[i] * divisor[j - i];
+                }
+            }
+
+            Array.Resize(ref remainder, divisorDegree);
+            return new FiniteFieldElement(remainder, field);
         }
         private static int mod(int k, int n) => ((k %= n) < 0) ? k + n : k;
 
---
+++ FFTest/DividePolyTest.cs
@@ -0,0 +1,40 @@
+﻿using FF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFTest
+{
+    public class DividePolyTest
+    {
+        [Test]
+        public void DivideWithMod()
+        {
+            var GF4 = new FiniteField(2, new int[] { 1, 1, 1 }, 2);
+            var element1 = new FiniteFieldElement(new int[] { 1, 2, 3, 4 }, GF4);
+            var element2 = new FiniteFieldElement(new int[] { 1, -1 }, GF4);
+            var mylt = element1 % element2;
+            Assert.That(mylt.Poly, Is.EqualTo(new int[] {10}));
+        }
+        [Test]
+        public void DoNotDivide()
+        {
+            var GF4 = new FiniteField(2, new int[] { 1, 1, 1 }, 2);
+            var element1 = new FiniteFieldElement(new int[] { 1, -1 }, GF4);
+            var element2 = new FiniteFieldElement(new int[] { 1, 2, 3, 4 }, GF4);
+            var mylt = element1 % element2;
+            Assert.That(mylt.Poly, Is.EqualTo(new int[] { 1, -1 }));
+        }
+        [Test]
+        public void DivideWithoutMod()
+        {
+            var GF4 = new FiniteField(2, new int[] { 1, 1, 1 }, 2);
+            var element1 = new FiniteFieldElement(new int[] { 1,2, 1 }, GF4);
+            var element2 = new FiniteFieldElement(new int[] { 1,1}, GF4);
+            var mylt = element1 % element2;
+            Assert.That(mylt.Poly, Is.EqualTo(new int[] {0}));
+        }
+    }
+}
